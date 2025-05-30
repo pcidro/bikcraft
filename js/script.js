22df8a662c92bf0cdcdcf90f38887ad5b6fa4be9
Paulo Cidro <paulocidro@gmail.com>
2025-05-30 08:15:58 -0300

    perguntas frequentes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Ativar Links do menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
@@ -10,3 +11,35 @@ function ativarLink(link) {
 }
 
 links.forEach(ativarLink);
+
+// Ativar Items do Orçamento
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+// Perguntas frequentes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa);
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
